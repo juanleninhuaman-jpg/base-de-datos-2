7d559be3f48940921d103a0d8db9bd580c558788
juanleninhuaman-jpg <juanleninhuaman@gmail.com>
2026-09-14 20:12:44 -0500

    Improve Google Drive URL transformation logic
    Updated PDF URL handling for Google Drive links to generate direct download URLs.

--- script.js
+++ script.js
@@ -76,7 +76,6 @@ const semanasInfo = {
       {
         actividad: 'ACTIVIDAD 02',
         pdfTitulo: 'Infografías Base de Datos SEM 1',
-        // REEMPLAZA esta URL con tu enlace real de Google Drive:
         pdfRuta: 'https://drive.google.com/file/d/1Y8pC6N4gLl6O6eOuul-4n6MnZxt2ig-Z/view?usp=sharing'
       }
     ],
@@ -114,12 +113,12 @@ function openWeekModal(weekName) {
         info.actividades.forEach(act => {
           let downloadUrl = act.pdfRuta;
 
-          // Transforma la URL solo si es una URL válida de Google Drive
+          // Extraer ID de Google Drive para generar la URL exacta de descarga directa
           if (act.pdfRuta && act.pdfRuta.includes('drive.google.com')) {
-            downloadUrl = act.pdfRuta
-              .replace('/view?usp=sharing', '&export=download')
-              .replace('/view', '&export=download')
-              .replace('?usp=sharing', '&export=download');
+            const match = act.pdfRuta.match(/\/d\/([a-zA-Z0-9_-]+)/);
+            if (match && match[1]) {
+              downloadUrl = `https://drive.google.com/uc?export=download&id=${match[1]}`;
+            }
           }
 
           contenidoHTML += `
